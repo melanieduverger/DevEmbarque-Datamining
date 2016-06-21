a56eb9512d695f80caa5d47a8d8b4da0da5b0439
qvacher <qvacher@gmail.com>
2016-06-21 10:25:48 +0200

    Ajout dessin lignes

--- Datamining/public/js/accueilCtrl.js
+++ Datamining/public/js/accueilCtrl.js
@@ -3,27 +3,76 @@ dataminingApp.controller('accueilCtrl', ['$scope', '$routeParams', 'uiGmapGoogle
 
 		$scope.map = {center: {latitude: 47.845114, longitude: 1.940584 }, zoom: 14 };
     $scope.options = {scrollwheel: true};
+    $scope.polylines = [];
 
-    $scope.marker = {
-      id: 0,
-      coords: {
-        latitude: 47.845114,
-        longitude: 1.960584
-      },
-    };
+    var rg = new RegExp(/\d{4,5}\.\d{5}[NOES]/g)
 
-        $scope.marker2 = {
-      id: 1,
-      coords: {
-        latitude: 47.825114,
-        longitude: 1.960584
+    var convertCoord = function(i){
+      if (!i.match(rg))
+        return null;
+      var value = parseFloat(i.substring(0,i.length-1));
+      var d = Math.floor(value / 100);
+      var m = value - d * 100;
+
+      return d + (m *60)/3600;
+    }
+
+    var createPolyline = function(i){
+      var latitude = convertCoord(test2['DATA'][i].Latitude);
+      var longitude = convertCoord(test2['DATA'][i].Longitude);
+     
+      var ret = {
+        latitude: latitude,
+        longitude: longitude
+      };
+      return ret;
+    }
+
+    var path = [];
+    var lastlat = 0, lastlong = 0;
+    for(var i =0; i<(test2['DATA'].length) ; i++){
+      var p = createPolyline(i);
+      if (isNaN(p.latitude) || isNaN(p.longitude))
+        continue;
+      if (p.latitude<-180 || p.latitude > 180 || p.longitude<-180 || p.longitude > 180  )
+        continue;
+      if (lastlat != 0 || lastlong != 0)
+      {
+          if (lastlat == p.latitude && lastlong == p.longitude)
+          continue;
       }
-    };
+      var toadd = true;
+        if (i != 0 && Math.abs(p.latitude - lastlat) > 0.2)
+          toadd = false;
+        if (i != 0 && Math.abs(p.longitude - lastlong) > 0.2)
+          toadd = false;
+     
+     if (!toadd)
+        continue;
+        lastlat = p.latitude;
+        lastlong = p.longitude;
+        path.push(p);
+    }
 
-    // uiGmapGoogleMapApi is a promise.
-    // The "then" callback function provides the google.maps object.
     GoogleMapApi.then(function(maps) {
-
+      $scope.polylines = [
+            {
+                id: 1,
+                path:  path,
+                stroke: { color: '#6060FB', weight: 1 },
+                editable: false,
+                draggable: false,
+                geodesic: true,
+                visible: true,
+                icons: [{
+                    icon: {
+                        path: google.maps.SymbolPath.FORWARD_OPEN_ARROW
+                    },
+                    offset: '25px',
+                    repeat: '50px'
+                }]
+            }
+        ];
     });
 
 		$scope.temp = {
@@ -35,6 +84,5 @@ dataminingApp.controller('accueilCtrl', ['$scope', '$routeParams', 'uiGmapGoogle
 			min: 20,
 			max: 60
 		};
-
 	}
 ]);
